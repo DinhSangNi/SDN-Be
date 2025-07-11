70068cc0eb851acecc932fb5e1a51f41351193aa
TruongDinhSang <sangprotqb@gmail.com>
2025-07-11 22:32:19 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get<string>('FE_URL') ?? 'http://localhost:3000',
     credentials: true,
   });
 
